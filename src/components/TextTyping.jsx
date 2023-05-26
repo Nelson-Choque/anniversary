6996a7b4d4e14a09fb62b45dffa108eac103d91c
Nelson Choque <nelsonchoque03@gmail.com>
2023-05-26 00:45:47 -0500

    sticht gif

--- src/components/TextTyping.jsx
+++ src/components/TextTyping.jsx
@@ -14,7 +14,7 @@ function TextTyping() {
       const changeText = setInterval(() => {
         if (counter === text.length - 1) {
           setTimeout(() => {
-            navigate("/happy-birthey");
+            // navigate("/happy-birthey");
           }, 2000);
           console.log("se termino");
           clearInterval(changeText);
@@ -30,56 +30,11 @@ function TextTyping() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-blue-800 flex flex-wrap items-center justify-center">
-      <div className="">
-        <div className="flex gap-4 justify-center">
-          <img
-            className="w-8 h-auto heart-day"
-            src="/images/heart.png"
-            alt=""
-          />
-          <img
-            className="w-8 h-auto heart-day"
-            src="/images/heart.png"
-            alt=""
-          />
-          <img
-            className="w-8 h-auto heart-day"
-            src="/images/heart.png"
-            alt=""
-          />
-          <img
-            className="w-8 h-auto heart-day"
-            src="/images/heart.png"
-            alt=""
-          />
-        </div>
-        <h2 className="p-4 transicion text-center text-white text-2xl">
-          {phrase}
-        </h2>
-        <div className="flex gap-4 justify-center">
-          <img
-            className="w-8 h-auto heart-day"
-            src="/images/heart.png"
-            alt=""
-          />
-          <img
-            className="w-8 h-auto heart-day"
-            src="/images/heart.png"
-            alt=""
-          />
-          <img
-            className="w-8 h-auto heart-day"
-            src="/images/heart.png"
-            alt=""
-          />
-          <img
-            className="w-8 h-auto heart-day"
-            src="/images/heart.png"
-            alt=""
-          />
-        </div>
-      </div>
+    <div className="bgText min-h-screen bg-blue-800 flex flex-wrap items-center justify-center">
+      <h2 className="p-4 transicion text-center text-white text-2xl">
+        {phrase}
+      </h2>
+      <img className="gif-stitch" src="/images/stich-transparent.gif" alt="" />
     </div>
   );
 }
